e02474299fe12db89e948c8a2a3b1176c8180869
Administrator <315541219@qq.com>
2020-03-20 09:37:59 +0800

    0320日历后台传值

--- src/main/java/com/sunjy/secret/boot/Controller/schedule/CalendarController.java
+++ src/main/java/com/sunjy/secret/boot/Controller/schedule/CalendarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import tk.mybatis.mapper.util.StringUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,20 +38,18 @@ public class CalendarController {
         return "/Schedule/calendar";
     }
 
-    @GetMapping("/getMap")
-    @ResponseBody
-    public JSONObject markJson(){
-        JSONObject o=new JSONObject();
-        o.put("2020-03-03","这天鸣人捣乱了");
-        o.put("2020-03-13","这天猪鹿蝶成型");
-        o.put("2020-03-21","鼬进木叶村了");
-        o.put("2020-04-02","我爱罗暴走");
-        o.put("2020-04-11","9：00中忍毕业典礼");
-        o.put("2020-05-23","音忍村袭击木叶");
-        o.put("2020-03-13","304");
-        o.put("2020-03-23","567");
 
+    @PostMapping("/saveData")
+    @ResponseBody
+   public  JSONObject saveData(){
+        String s= request.getParameter("mapdata") ;
+        JSONObject o = new JSONObject();
+        if(StringUtil.isNotEmpty(s)) {
+            o.put("msg", "ok");
+            o.put("data", JSONObject.parseObject(s));
+        }else{
+            o.put("msg", "no");
+        }
         return o;
     }
-
 }
